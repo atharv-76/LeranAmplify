ef3f4b3b5a49fb956db4113369f7ccf825ff9c8d
Ashwini Patil <ashwinipatil100313@gmail.com>
2025-01-17 22:37:29 +0530

    frontend done

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,9 @@
 import RegisterPage from "@/components/RegisterPage";
-import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  
   return (
     <>
     <div className="border border-gray-100 justify-center text-center">
