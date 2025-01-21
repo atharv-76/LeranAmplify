772dc504a402889e6f5ec9b747136090dad9c089
Ashwini Patil <161121391+ashwini-1013@users.noreply.github.com>
2025-01-21 13:01:07 +0530

    Update LoginForm.jsx

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
 
 export default function LoginForm() {
   const [email, setEmail] = useState("");
@@ -28,7 +29,7 @@ export default function LoginForm() {
       }
 
       toast.success('Login Successful', {
-        position: toast.POSITION.TOP_RIGHT,
+        position: 'top-right',
         autoClose: 5000,
         hideProgressBar: false,
         closeOnClick: true,
@@ -40,7 +41,7 @@ export default function LoginForm() {
       router.replace("dashboard");
     } catch (error) {
       toast.error('Login Unsuccessful', {
-        position: toast.POSITION.TOP_RIGHT,
+        position: 'top-right',
         autoClose: 5000,
         hideProgressBar: false,
         closeOnClick: true,
